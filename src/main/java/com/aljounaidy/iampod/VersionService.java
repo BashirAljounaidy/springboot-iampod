37f11f1ce9817196ec94c98bf75d44591410098b
Mohamad Bashir Aljounaidy <bma.963@gmail.com>
2023-11-18 17:01:50 +0400

    upgrade app to v3.6

--- src/main/java/com/aljounaidy/iampod/VersionService.java
+++ src/main/java/com/aljounaidy/iampod/VersionService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class VersionService {
 
-    private String version = "3.5";
+    private String version = "3.6";
 
     public String getVersion() {
         return version;
