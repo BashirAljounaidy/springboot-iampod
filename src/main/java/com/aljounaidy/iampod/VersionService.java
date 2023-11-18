718c7f5d214b70eb7710542230d1fe4e0c6b6ccb
Mohamad Bashir Aljounaidy <bma.963@gmail.com>
2023-11-19 00:00:51 +0400

    latest v5

--- src/main/java/com/aljounaidy/iampod/VersionService.java
+++ src/main/java/com/aljounaidy/iampod/VersionService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class VersionService {
 
-    private String version = "4.9";
+    private String version = "5";
 
     public String getVersion() {
         return version;
