4040ebc4a563bc2be227cc5d5a978d56ab2e9600
Mohamad Bashir Aljounaidy <bma.963@gmail.com>
2023-11-19 18:34:21 +0400

    latest v5.4 improve docker file

--- src/main/java/com/aljounaidy/iampod/VersionService.java
+++ src/main/java/com/aljounaidy/iampod/VersionService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class VersionService {
 
-    private String version = "5.6";
+    private String version = "5.8";
 
     public String getVersion() {
         return version;
